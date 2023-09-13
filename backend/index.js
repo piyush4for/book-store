033256afc8f53b8a80a4cacb3824f4cc95efbba7
piyush4for <piyush4for@gmail.com>
2023-09-13 16:32:25 +0530

    backend api created

---
+++ backend/index.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import { PORT, mongoDBURL } from './config.js';
+import mongoose from 'mongoose';
+import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
+
+const app = express();
+
+app.use(express.json());
+
+// Middleware for handling CORS POLICY
+//  Allow Custom Origins
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+  })
+);
+
+app.get('/', (request, response) => {
+  console.log(request);
+  return response.status(234).send('Welcome To piyush book store');
+});
+
+app.use('/books', booksRoute);
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log('App connected to database');
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
